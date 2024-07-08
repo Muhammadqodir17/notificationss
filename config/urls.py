bedd12bc3e08cf1a018aeb957fa46e2ca1167ab9
Muhammadqodir <155760560+Muhammadqodir17@users.noreply.github.com>
2024-07-08 19:28:55 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from rest_framework.permissions import AllowAny
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="UzMovie API",
+        title="Notifications",
 
         default_version='v1', ),
     public=True,
