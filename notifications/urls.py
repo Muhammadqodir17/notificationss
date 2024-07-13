eee524143b150f44cc8d99b9d91d3e2b8c395712
Muhammadqodir <km1728.uz@gmail.com>
2024-07-13 17:14:38 +0500

    added

--- notifications/urls.py
+++ notifications/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('notification/', NotificationViewSet.as_view({'post': 'send_notifications'})),
     path('notification/follow/', NotificationForFollowViewSet.as_view({'post': 'send_follow_notification'})),
     path('notification/<int:pk>/', NotificationViewSet.as_view({'post': 'get_by_id'})),
-    path('notification/<int:pk>/', NotificationViewSet.as_view({'post': 'get_all'}))
+    path('notification/get/', NotificationViewSet.as_view({'post': 'get_all'}))
 ]
